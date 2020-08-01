2681be523988b958b1ce5765e3f2e727243406b7
xeb35103 <xeb35103@users.noreply.github.com>
2020-08-01 12:26:32 +0900

    add setups

--- setup_first.sh
+++ setup_first.sh
@@ -12,10 +12,15 @@ LANG=C xdg-user-dirs-gtk-update
 sudo apt install git etckeeper silversearcher-ag mg jed meld
 
 # dotfiles
-git clone https://github.com/xeb35103/dotfiles.git
-cd dotfiles/
-git checkout ubuntu18.04
-./linkAll.sh
+cd ~
+if [ -d ${HOME}/dotfiles ]; then
+    echo "dotfiles is already exist."
+else
+    git clone https://github.com/xeb35103/dotfiles.git
+    cd dotfiles/
+    git checkout ubuntu18.04
+    ./linkAll.sh
+fi;
 
 # vscode
 sudo apt install curl
@@ -34,25 +39,8 @@ sudo apt install google-chrome-stable
 wget https://raw.githubusercontent.com/PX4/Devguide/v1.9.0/build_scripts/ubuntu_sim_ros_melodic.sh
 source ubuntu_sim_ros_melodic.sh
 
-# ROS
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt update
-sudo apt install ros-melodic-desktop-full 
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo rosdep init
-rosdep update
-
-sudo apt install python-catkin-tools
-source /opt/ros/melodic/setup.bash
-mkdir -p ~/ros_catkin_ws/melodic/src
-cd ~/ros_catkin_ws/melodic
-catkin build
-source devel/setup.bash
-#catkin init
-
-
+#
+bash setup_ros.sh
 
 
 
